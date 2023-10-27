9c50f769befd6a21d175f95c997f0641226780c6
Brett OdgersJewell <Brett.OdgersJewell@insearch.edu.au>
2023-10-27 09:14:43 +0800

    update module

--- variables.tf
+++ variables.tf
@@ -7,8 +7,8 @@ variable "diag_object" {
   description = "(Required) Contains the diagnostics setting object."
   type = map(object({
     resource_id = list(string)
-    enabled_log = list(tuple([string, bool, number]))
-    metric      = list(tuple([string, bool, number]))
+    enabled_log = list(tuple([string, bool, bool, number]))
+    metric      = list(tuple([string, bool, bool, number]))
   }))
 }
 
@@ -34,7 +34,6 @@ variable "tags" {
 }
 
 variable "dependencies" {
-  type        = string
   description = "List of dependecies modules or resources"
   default     = null
 }
